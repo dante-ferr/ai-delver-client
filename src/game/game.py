7ea31f479a582259218352ec7fe2909e85f8267a
Dante Teles Ferreira <dante@DESKTOP-CPA7H4J.>
2025-03-25 21:42:37 -0300

    fix camera centering bug

--- src/game/game.py
+++ src/game/game.py
@@ -18,20 +18,19 @@ class Game:
         display = pyglet.display.get_display()
         screen = display.get_screens()[0]
 
-        self.window = pyglet.window.Window(fullscreen=False)
+        self.window = pyglet.window.Window(fullscreen=False, resizable=False)
+
+        self.camera: None | Camera = None
 
         def _callback(dt):
             self.window.maximize()
-            pyglet.clock.schedule_once(lambda dt: self._lock_window_size(), 0.01)
+            pyglet.clock.schedule_once(self._on_screen_maximize_interval, 0.01)
 
         pyglet.clock.schedule_once(_callback, 0.1)
 
         # Initialize camera
         self.entities_controller = entities_controller_factory(space)
         self.delver = self.entities_controller.get_entity_by_name("delver")
-
-        self.camera = Camera(self.window, start_zoom=0.5, min_zoom=0.25, max_zoom=2)
-        self.camera.start_following(self.delver)
 
         # Initialize tilemap
         # self.tilemap_renderer = tilemap_factory()
@@ -47,6 +46,12 @@ class Game:
             self.keys,
             on_mouse_scroll=self.controls.on_mouse_scroll,
         )
+
+    def _on_screen_maximize_interval(self, dt):
+        self._lock_window_size()
+
+        self.camera = Camera(self.window, start_zoom=0.5, min_zoom=0.25, max_zoom=2)
+        self.camera.start_following(self.delver)
 
     def _lock_window_size(self):
         """Locks the window size completely (even on Linux)"""
@@ -71,8 +76,9 @@ class Game:
         self.delver.update(dt)
         self.controls.update(dt)
 
-        with self.camera:
-            pass
+        if self.camera is not None:
+            with self.camera:
+                pass
 
         space.step(dt)
 
