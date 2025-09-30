2a951efcc169d8a5b02033efb38c8ff1c2adb4ff
Dante <dante.tf5@gmail.com>
2025-09-30 20:29:09 -0300

    limit angle on vector_to_angle function

--- src/utils/vector_to_angle.py
+++ src/utils/vector_to_angle.py
@@ -1,8 +1,12 @@
 import math
+from typing import Sequence
 
 
-def vector_to_angle(vector):
+def vector_to_angle(vector: Sequence[float]) -> float:
+    """Converts a 2D vector to an angle in degrees (0-360)."""
     x, y = vector
     angle_radians = math.atan2(y, x)
     angle_degrees = math.degrees(angle_radians)
-    return angle_degrees
+
+    # Ensure the angle is in the range [0, 360)
+    return (angle_degrees + 360) % 360
